38744b11e72a0bafa92d209674f3d8f92708d7be
Zibin Dong <zibindong@ZibindeMacBook-Pro.local>
2019-05-13 00:07:49 -0700

    database finished

--- website/server.js
+++ website/server.js
@@ -3,27 +3,36 @@ const app = express();
 
 app.use(express.static('static_files'));
 
-const fakeDatabase = {
-  'Philip': {category: 'Education', subscribers: '2.3M', upvote: '99%'},
-  'Viki': {category: 'Beauty & Fashion', subscribers: '1.9M', upvote: '95%'},
-  'Benny': {category: 'Music', subscribers: '1.9M', upvote: '93%'},
-};
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('youtuber.db');
 
 app.get('/youtuber', (req, res) => {
-  const allYoutuber = Object.keys(fakeDatabase); // returns a list of object keys
-  console.log('allUsernames is:', allYoutuber);
-  res.send(allYoutuber);
+  db.all('SELECT title FROM youtuber_data', (err, rows) =>{
+    console.log(rows);
+    const allYoutuber = rows.map(e => e.title);
+    console.log(allYoutuber);
+    res.send(allYoutuber);
+  });
 });
 
 app.get('/youtuber/:youtuberid', (req, res) => {
   const nameToLookup = req.params.youtuberid; // matches ':userid' above
-  const val = fakeDatabase[nameToLookup];
-  console.log(nameToLookup, '->', val); // for debugging
-  if (val) {
-    res.send(val);
-  } else {
-    res.send({}); // failed, so return an empty object instead of undefined
-  }
+  db.all(
+    'SELECT * FROM youtuber_data WHERE title = $title',
+    {
+      $title: nameToLookup
+    },
+    (err, rows) => {
+      console.log(rows);
+      if(rows.length > 0){
+        res.send(rows[0]);
+      }
+      else {
+        res.send({});
+      }
+    }
+  );
+
 });
 
 app.listen(3000, () => {
